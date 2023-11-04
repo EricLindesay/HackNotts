63241ca77bcbce3688e9abfeecd8074fbc44eca5
Eric Lindesay <psyel1@nottingham.ac.uk>
2023-11-04 12:49:37 +0000

    Files

---
+++ routing.py
@@ -0,0 +1,12 @@
+# Get the number of inputs
+
+# Get the number of outputs
+
+# Get the gates we need - put this in a nxn grid (as best as possible)
+
+
+inputs = 3
+outputs = 2
+
+
+blocks = [[[]]]
