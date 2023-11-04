60450e3f5358897c31833c0003eca9af5dcd078c
Eric Lindesay <psyel1@nottingham.ac.uk>
2023-11-04 13:02:17 +0000

    constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+AND = 0
+OR = 1
+NOT = 2
+NOR = 3
+NAND = 4
+DFF = 5
+
+string_constant = {"AND": AND,
+                   "OR": OR,
+                   "NOT": NOT,
+                   "NOR": NOR,
+                   "NAND": NAND,
+                   "DFF": DFF,
+                   }
+
+constant_string = {AND: "AND",
+                   OR: "OR",
+                   NOT: "NOT",
+                   NOR: "NOR",
+                   NAND: "NAND",
+                   DFF: "DFF",
+                   }
