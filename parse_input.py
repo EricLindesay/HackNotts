dacbfdb2811a92b157aecd14ad365a5f413ad994
Eric Lindesay <psyel1@nottingham.ac.uk>
2023-11-07 02:43:52 +0000

    Update README - After submission

--- parse_input.py
+++ parse_input.py
@@ -1,7 +1,7 @@
 # array of nodes, index of which is the id
 import json
 import constants
-from classes import * 
+from classes import *
 nodelist = []
 
 # class Node:
@@ -14,9 +14,12 @@ nodelist = []
 #     #inputs: list[int] = []
 #     input_wires = []
 #     output_wire: int = 0
+
+
 class read_input:
     def __init__(self) -> None:
         pass
+
     def read_inputs(self, path):
         f = open(path)
         loaded = json.load(f)
@@ -25,7 +28,8 @@ class read_input:
         inputs = []
         for portname in ports:
             port = ports[portname]
-            if(port["direction"] != "input"): continue
+            if (port["direction"] != "input"):
+                continue
             bits = port["bits"]
             for bit in bits:
                 opt = Input()
@@ -33,7 +37,8 @@ class read_input:
                 opt.wire = int(bit)
                 inputs.append(opt)
         return inputs
-    def read_outputs(self,path):
+
+    def read_outputs(self, path):
         f = open(path)
         loaded = json.load(f)
         ports = loaded["modules"]["counter"]["ports"]
@@ -41,7 +46,8 @@ class read_input:
         inputs = []
         for portname in ports:
             port = ports[portname]
-            if(port["direction"] != "output"): continue
+            if (port["direction"] != "output"):
+                continue
             bits = port["bits"]
             for bit in bits:
                 opt = Output()
@@ -49,12 +55,12 @@ class read_input:
                 opt.wire = int(bit)
                 inputs.append(opt)
         return inputs
+
     def read_gates(self, path):
         f = open(path)
         loaded = json.load(f)
         cells = loaded["modules"]["counter"]["cells"]
         tempNodes = []
-
 
         for cellname in cells:
             cell = cells[cellname]
@@ -64,26 +70,30 @@ class read_input:
             input_wires = []
             output_wire = 0
             for connection in connections:
-                if(cell["port_directions"][connection] == "input"): input_wires.append(connections[connection][0])
-                if(cell["port_directions"][connection] == "output"): output_wire = int(connections[connection][0])
+                if (cell["port_directions"][connection] == "input"):
+                    input_wires.append(connections[connection][0])
+                if (cell["port_directions"][connection] == "output"):
+                    output_wire = int(connections[connection][0])
             newNode.input_wires = input_wires
             newNode.output_wire = output_wire
             newNode.id = len(tempNodes)  # Assign the node's id
             tempNodes.append(newNode)
-            #print(newNode.id, newNode.node_type, newNode.temp_input_wires, newNode.temp_output_wire)
-        
+            # print(newNode.id, newNode.node_type, newNode.temp_input_wires, newNode.temp_output_wire)
+
         # for node1 in tempNodes:
         #     for node2 in tempNodes:
         #         if(node1.id == node2.id): continue
         #         if(node1.temp_output_wire in node2.temp_input_wires): node1.outputs.append(node2.id)
         #         if(node2.temp_output_wire in node1.temp_input_wires): node1.inputs.append(node2.id)
         #     print(node1.id, node1.outputs, node1.inputs)
-        
+
         for node in tempNodes:
-            nodelist.append( node)
+            nodelist.append(node)
         return nodelist
+
+
 reader = read_input()
-#nodes = reader.read_gates("./yosys/opt6.json")
+# nodes = reader.read_gates("./yosys/opt6.json")
 inputs = reader.read_outputs("./yosys/opt6.json")
 
 for node in inputs:
